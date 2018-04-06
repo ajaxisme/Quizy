20db135b0dbdee98abe2bb875268643a24ec8b37
Abhinav Jha <abjha@mediamath.com>
2018-04-05 22:15:09 -0400

    first commit - quizy

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Title from './components/TitleComponent';
+import OuterLayout from './components/OuterLayoutComponent';
+
+class App extends Component {
+  render() {
+    return (
+      <OuterLayout />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/OuterLayoutComponent/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Title from '../TitleComponent';
+
+class OuterLayoutComponent extends Component {
+	render() {
+		return (
+			<Title />
+		);
+	}
+}
+
+export default OuterLayoutComponent;
